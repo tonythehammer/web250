dddb5f88ecd1c2f790c06310a03669b5bc4b5cf0
Anthony Harwood <114599727+tonythehammer@users.noreply.github.com>
2023-11-06 13:54:05 -0500

    ready to add to site

---
+++ assignments/asgn10/private/dbCredentials.php
@@ -0,0 +1,13 @@
+<?php 
+
+// define("DB_SERVER", "localhost");
+// define("DB_USER","tony");
+// define("DB_PASS","Awh28806AWH!");
+// define("DB_NAME","sabirds");
+
+define("DB_SERVER", "pdb1048.awardspace.net");
+define("DB_USER","4373591_web250");
+define("DB_PASS","Awh28806AWH!");
+define("DB_NAME","4373591_web250");
+
+?>
